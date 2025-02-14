7601516ef29e37585b76b152118ad311946ef87b
Aidan Puricelli <aidanpuricelli@gmail.com>
2025-02-13 21:15:37 -0600

    gitrog

--- run_project.bat
+++ run_project.bat
@@ -1,5 +1,23 @@
 @echo off
 cd /d "%~dp0"
+
+:: Check if node_modules exists, if not, run npm install
+if not exist "node_modules" (
+    echo node_modules not found. Running npm install...
+    npm install
+)
+
+:: Start frontend
 start cmd /k "npm run start"
+
+:: Navigate to server directory
 cd server
+
+:: Check if server node_modules exists, if not, run npm install
+if not exist "node_modules" (
+    echo node_modules not found in server. Running npm install...
+    npm install
+)
+
+:: Start server
 start cmd /k "node server.js"
